c25f48237978eb6bc7ec3d93446208fcae20b6c1
Thierry Chau <thierry.chau@gmail.com>
2023-07-25 03:46:46 +0000

    initial commit

--- app.rb
+++ app.rb
@@ -2,8 +2,26 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  
+  erb(:square)
+end
+
+get("/square/new") do
+
+  erb(:square)
+end
+
+get("/square/results") do
+  
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+
+  erb(:square)
+end
+
+get("/square_root/results") do
+  
+  erb(:square_results)
 end
