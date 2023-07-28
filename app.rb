22f338aed65e8590cabaff17d6c5e6cdc535c81f
Thierry Chau <thierry.chau@gmail.com>
2023-07-28 16:39:51 +0000

    rake grade

--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
   
@@ -18,10 +25,41 @@ end
 
 get("/square_root/new") do
 
-  erb(:square)
+  erb(:square_root)
 end
 
 get("/square_root/results") do
   
-  erb(:square_results)
+  erb(:square_root_results)
+end
+
+get("/payment/new") do
+
+  erb(:payment)
+end
+
+get("/payment/results") do
+  @apr = params.fetch("apr").to_f / 100
+  @years = params.fetch("years").to_i
+  @principal = params.fetch("principal").to_i
+
+  @number_periods = 12*@years
+  @rate_per_period = @apr/12
+
+  @payment = (@rate_per_period * @principal) / (1 - (1 + @rate_per_period)**(-@number_periods))
+
+  erb(:payment_results)
+end
+
+get("/random/new") do
+
+  erb(:random)
+end
+
+get("/random/results") do
+  @minimum = params.fetch("minimum").to_f
+  @maximum = params.fetch("maximum").to_f
+  @random = rand(@minimum..@maximum)
+
+  erb(:random_results)
 end
